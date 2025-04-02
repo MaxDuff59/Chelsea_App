cf1cb8439be3a27e296eca24678fb540da32c4d1
MaxDuff59 <maxence.duffuler@epfedu.fr>
2025-04-02 08:25:54 +0200

    Modification of main.py

--- main.py
+++ main.py
@@ -328,7 +328,10 @@ if value_tab == "General":
         st.markdown("""<h6 align=center>Coach's Note</h6>""",unsafe_allow_html=True)
 
         cols = st.columns([2,4])
-        cols[0].image("images/Enzo_maresca.png", width=150)
+        try:
+            cols[0].image("images/Enzo_maresca.png", width=150)
+        except:
+            pass
 
         cols[1].markdown(
             """
